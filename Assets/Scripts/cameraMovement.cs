38e5f17c80f6b14cf254a4fc84d1d9b32b0768bf
dianaalina99 <diana-alina.dobrescu@student.manchester.ac.uk>
2019-11-20 16:53:10 +0000

    Initial commit

---
+++ Assets/Scripts/cameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+
+    private Vector3 offset;
+    public GameObject sphere;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Store the distance between the camera and the sphere.
+        offset = transform.position - sphere.transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //Set camera follow the sphere.
+        transform.position = sphere.transform.position + offset;
+        
+    }
+}
---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody sphere;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        sphere = GetComponent<Rigidbody>();
+        
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 movementVector = new Vector3(moveX,0.0f , moveZ);
+        sphere.AddForce(movementVector * speed);
+        
+    }
+
+   
+}
