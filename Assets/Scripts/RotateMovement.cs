b92d3c1d45728c0f451869e5189165aa4c1cea18
dianaalina99 <diana-alina.dobrescu@student.manchester.ac.uk>
2019-11-27 03:16:34 +0000

    Adding coin with rotate movement

---
+++ Assets/Scripts/RotateMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMovement : MonoBehaviour
+{
+
+    public float turnSpeed = 100f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.right * turnSpeed * Time.deltaTime);
+        
+    }
+}
